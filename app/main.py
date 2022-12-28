710ceba100b2d20621dffd0f0c7f290dee9de04e
Siebe Van Campenhout <67122790+SiebeVC@users.noreply.github.com>
2022-12-28 17:28:25 +0100

    Working V1

---
+++ app/main.py
@@ -0,0 +1,31 @@
+from io import StringIO
+
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+import pandas as pd
+import base64
+from starlette.responses import FileResponse, Response
+
+from app.rewind import make_rewind
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    with open("files/form.html", 'r') as form:
+        form_d = form.read()
+    return form_d
+
+
+@app.post("/rewind")
+def rewind(name, file: UploadFile = File(...)):
+    text = file.file.read().decode('utf-8')
+    # read file wiht pandas
+    df = pd.read_csv(StringIO(text), sep=',', parse_dates=True)
+    return Response(content=make_rewind(df, name), media_type="text/html")
+
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
---
+++ app/rewind.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+
+def make_rewind(df, name):
+    with open("files/index.html", 'r') as temp:
+        template = temp.read()
+
+    session_count = len(df)
+    template = template.replace("=SESSIONS=", str(session_count))
+
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['ShortDate'] = df['Date'].dt.strftime('%Y-%m-%d')
+    day_count = len(df['ShortDate'].unique())
+    template = template.replace("=DAYS=", str(day_count))
+
+    df['Time'] = pd.to_timedelta(df['Time'])
+    total_time = df['Time'].sum()
+    seconds = total_time.total_seconds()
+    days, rem = divmod(seconds, 86400)
+    hours, rem = divmod(rem, 3600)
+    minutes, seconds = divmod(rem, 60)
+    template = template.replace("=TIME=", f"{int(days)}d {int(hours)}h {int(minutes)}m")
+
+    calories = round(df['Calories'].sum())
+    template = template.replace("=CALORIES=", str(calories))
+
+    average_heart_rate = round(df['Avg HR'].mean())
+    template = template.replace("=AVGHEART=", str(average_heart_rate))
+
+    max_heart_rate = round(df['Max HR'].max())
+    template = template.replace("=MAXHEART=", str(max_heart_rate))
+
+    def reformat_height(s):
+        s = s.replace('.', '')
+        s = s.replace("--", '0')
+        return float(s)
+
+    df["Max Height"] = df["Max Elevation"].apply(reformat_height).astype(float)
+    highest_point = round(df['Max Height'].max())
+    template = template.replace("=MAXHEIGHT=", str(highest_point))
+
+    total_height = round(df['Total Ascent'].apply(reformat_height).astype(float).sum())
+    template = template.replace("=TOTALHEIGHT=", str(total_height))
+
+    activity_types = {**df['Activity Type'].value_counts()}
+    i = 1
+    for k, v in activity_types.items():
+        if i > 4:
+            break
+        template = template.replace(f"=ACT{i}=", str(k) + " - " + str(v))
+        i += 1
+
+    while i <= 4:
+        template = template.replace(f"=ACT{i}=", "")
+        i += 1
+
+    template = template.replace("=NAME=", name)
+
+    return str(template)
